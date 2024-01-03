14f2d9b26af4110722f8ad8a5a29d264c69bda3d
Anton Aparin <anton@antonaparin.com>
2024-01-03 18:58:04 +0100

    Remove results from source code

--- src/main.rs
+++ src/main.rs
@@ -22,8 +22,6 @@ fn main() {
         .collect::<Vec<_>>();
     println!("{:?}", working_sites);
 
-    // None
-
     let resolver = Resolver::new(ResolverConfig::default(), ResolverOpts::default()).unwrap();
     let with_ip = tlds
         .par_iter()
@@ -46,18 +44,4 @@ fn main() {
         })
         .collect::<Vec<_>>();
     println!("{:?}", with_ip);
-
-    // [
-    //     ("AI", "209.59.119.34"),
-    //     ("ARAB", "127.0.53.53"),
-    //     ("CM", "195.24.205.60"),
-    //     ("MUSIC", "127.0.53.53"),
-    //     ("TK", "217.119.57.22"),
-    //     ("UZ", "91.212.89.8"),
-    //     ("VA", "2a01:b8:0:1:212:77:0:2"),
-    //     ("WS", "64.70.19.33"),
-    //     ("XN--L1ACC", "180.149.98.234"),
-    //     ("XN--MXTQ1M", "127.0.53.53"),
-    //     ("XN--NGBRX", "127.0.53.53"),
-    // ]
 }
